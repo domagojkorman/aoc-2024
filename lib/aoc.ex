070aea00d20f7219254f1fc6e50aa94d051ebf1b
Domagoj Korman <domagojkorman93@gmail.com>
2024-12-16 19:29:25 +0100

    Add time output for all solutions

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -35,15 +35,24 @@ defmodule Aoc do
     real_index = floor(index / 2) + 1
     part = if rem(index, 2) == 0, do: "A", else: "B"
     seconds = (time / 1_000_000) |> Float.round(3)
-    IO.puts("Solution \##{real_index}#{part} in #{seconds}s: #{result}")
+
+    IO.puts(
+      "Solution \##{real_index |> Integer.to_string() |> String.pad_leading(2, "0")}#{part} in #{seconds}s: #{result}"
+    )
   end
 
   def run_solutions do
-    @modules
-    |> Enum.flat_map(&create_tasks/1)
-    |> Enum.map(&run_task/1)
-    |> Enum.map(&Task.await(&1, :timer.seconds(10)))
-    |> Enum.with_index(0)
-    |> Enum.each(&print_output/1)
+    {time, _} =
+      :timer.tc(fn ->
+        @modules
+        |> Enum.flat_map(&create_tasks/1)
+        |> Enum.map(&run_task/1)
+        |> Enum.map(&Task.await(&1, :timer.seconds(10)))
+        |> Enum.with_index(0)
+        |> Enum.each(&print_output/1)
+      end)
+
+    seconds = (time / 1_000_000) |> Float.round(3)
+    IO.puts("All solutions done in #{seconds}s")
   end
 end
